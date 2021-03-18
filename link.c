4bea3d95de92626bfb6cb6b0ed00cace957929b7
Mark Smith <mark.smith@ablevm.org>
2021-03-18 12:24:15 +0100

    Unlock link before calling link post shim

--- link.c
+++ link.c
@@ -44,9 +44,8 @@ able_link_send_done(able_link_t *link, uint16_t size) {
 		atomic_store(&link->sl, 0);
 		return y;
 	}
-	y = able_link_node_post_shim(n, &p->e);
 	atomic_store(&link->sl, 0);
-	return y;
+	return able_link_node_post_shim(n, &p->e);
 }
 
 int
