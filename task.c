e30858a046d1581e990e9d18a4a2877b7830f6f2
Mark Smith <mark.smith@ablevm.org>
2022-05-05 19:07:46 +0200

    Declare internal task exec shim static

--- task.c
+++ task.c
@@ -8,8 +8,8 @@ able_task_exec(able_task_t *task) {
 	return y;
 }
 
-void *
-able_task_exec_shim(void *task) {
+static void *
+task_exec_shim(void *task) {
 	able_task_exec(task);
 	return NULL;
 }
@@ -19,7 +19,7 @@ able_task_fork_exec(able_task_t *task) {
 	pthread_attr_t thread_attr;
 	pthread_attr_init(&thread_attr);
 	pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
-	pthread_create(&task->c, &thread_attr, able_task_exec_shim, task);
+	pthread_create(&task->c, &thread_attr, task_exec_shim, task);
 	pthread_attr_destroy(&thread_attr);
 	return 0;
 }
